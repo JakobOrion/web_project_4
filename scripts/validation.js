9f4a2f5aab4fb51b534fbc11ec1850343a0c9430
Jakob Pfefferkorn <jakoborion@gmail.com>
2020-11-23 09:42:25 -0500

    feat: setting up validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,42 +1,65 @@
-function showErrorMessage() {
-  const error = document.querySelector
+function showErrorMessage(input, form, {errorClass, inputErrorClass, ...rest}) {
+  const error = document.querySelector('#' + input.id + '-error');
+  error.textContent = input.validationMessage;
+
+  error.classList.add(errorClass);
+  input.classList.add(inputErrorClass);
 }
 
 
+function hideErrorMessage(input, form, {errorClass, inputErrorClass, ...rest}) {
+  const error = document.querySelector('#' + input.id + '-error');
+  error.textContent = '';
+
+  error.classList.remove(errorClass);
+  input.classList.remove(inputErrorClass);
+}
+
 function checkInputValidity(input, form, rest) {
   if(input.validity.valid) {
-    // hideErrorMessage()
+    hideErrorMessage(input, form, rest);
   } else {
     showErrorMessage(input, form, rest);
   }
 }
 
+function toggleButtonState(inputs, button, {inactiveButtonClass, ...rest}) {
+  const isValid = inputs.every((input) => input.validity.valid);
 
-function enableValidaton({formSelector, inputSelector, submitButtonSelector, ...rest}) {
-  const forms = [...document.querySelectorAll(settings.formSelector)];
+  if(isValid) {
+    button.classList.remove(inactiveButtonClass);
+  } else {
+    button.classList.add(inactiveButtonClass);
+  }
+
+}
+
+
+function enableValidation({formSelector, inputSelector, submitButtonSelector, ...rest}) {
+  const forms = [...document.querySelectorAll(formSelector)];
 
   forms.forEach((form) => {
     form.addEventListener('submit', ((e) => {
       e.preventDefault()
     }))
 
-    const inputs = [...form.querySelectorAll(settings.inputSelector)];
-    const button = form.querySelector(settings.submitButtonSelector);
+    const inputs = [...form.querySelectorAll(inputSelector)];
+    const button = form.querySelector(submitButtonSelector);
 
     inputs.forEach((input) => {
-      input.addEventListener('input' , () => {
+      input.addEventListener('input', () => {
         checkInputValidity(input, form, rest);
-        // toggleButtonState;
+        toggleButtonState(inputs, button, rest);
       })
     })
 
   })
 }
 
-enableValidaton({
+enableValidation({
     formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
+    inputSelector: ".form__input",
+    submitButtonSelector: ".form__submit",
     inactiveButtonClass: "popup__button_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible"
