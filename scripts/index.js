d0b7426a2028eff82e9291d9ea0e21a3283a0646
Jakob Pfefferkorn <jakoborion@gmail.com>
2020-11-23 12:14:37 -0500

    refactor: move inital places array to its own file

--- scripts/index.js
+++ scripts/index.js
@@ -30,34 +30,9 @@ const imageLinkInput = document.querySelector('.form__input_type_url');
 const cardTemplate = document.querySelector('.photo-card-template').content.querySelector('.photo-card');
 const list = document.querySelector('.photo-cards__group');
 
-
-// Initial places
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-];
+// Image popup
+const popupPhoto = imagePopup.querySelector('.popup__image');
+const popupPhotoTitle = imagePopup.querySelector('.popup__image-title');
 
 // Inital cards on page load
 initialCards.forEach((data) => {
@@ -90,9 +65,6 @@ function addCard(name, link) {
 
   // Image click listener
   cardImage.addEventListener('click', () => {
-    const popupPhoto = imagePopup.querySelector('.popup__image');
-    const popupPhotoTitle = imagePopup.querySelector('.popup__image-title');
-
     popupPhoto.src = link;
     popupPhotoTitle.textContent = name;
 
